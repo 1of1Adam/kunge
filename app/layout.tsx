05750a10bc4bc716a54a47995fd70cc22e6bc05f
adam <1of1adampeng@gmail.com>
2026-01-08 06:36:34 -0500

    feat: add clerk auth gating

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import './global.css';
 import { Inter } from 'next/font/google';
+import { ClerkProvider } from '@clerk/nextjs';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -8,10 +9,12 @@ const inter = Inter({
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
-      <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
-      </body>
-    </html>
+    <ClerkProvider signInUrl="/sign-in" signUpUrl="/sign-up">
+      <html lang="en" className={inter.className} suppressHydrationWarning>
+        <body className="flex flex-col min-h-screen">
+          <RootProvider>{children}</RootProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
